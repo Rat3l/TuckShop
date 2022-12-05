cc4e11a74687437490e602d25a3f13de16958055
Rat3l <36169286+Rat3l@users.noreply.github.com>
2022-12-05 10:24:04 +0200

    Added noindexing. To prevent website from showing on google search

--- SyntechShop/Inventory.php
+++ SyntechShop/Inventory.php
@@ -29,6 +29,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="robots" content="noindex">
 	<title>Inventory</title>
 	<link rel="shortcut icon" href="images/favicon.ico">
 	<link rel="stylesheet" type="text/css" href="css/stylesheet.css">

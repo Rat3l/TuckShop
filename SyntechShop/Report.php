e6b04e94ffe58b8ba23a38803729f3ae395f7ef8
Rat3l <36169286+Rat3l@users.noreply.github.com>
2022-12-05 10:25:40 +0200

    Added noindexing. To prevent website from showing on google search

--- SyntechShop/Report.php
+++ SyntechShop/Report.php
@@ -22,6 +22,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="robots" content="noindex">
 	<title>Report</title>
 	<link rel="shortcut icon" href="images/favicon.ico">
 	<link rel="stylesheet" type="text/css" href="css/stylesheet.css">

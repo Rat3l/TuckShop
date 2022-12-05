3902b75ab1e6b9f1d47eee659b3631e22bb44afd
Rat3l <36169286+Rat3l@users.noreply.github.com>
2022-12-05 10:22:57 +0200

    Added noindexing. To prevent website from showing on google search

--- SyntechShop/ControlPanel.php
+++ SyntechShop/ControlPanel.php
@@ -24,6 +24,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="robots" content="noindex">
 	<title>Sign Up</title>
     <link rel="shortcut icon" href="images/favicon.ico">
 	<link rel="stylesheet" type="text/css" href="css/stylesheet.css">

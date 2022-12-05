bb9180c2c8f47b2abd8a6927b09801e66cf9d482
Rat3l <36169286+Rat3l@users.noreply.github.com>
2022-12-05 10:23:23 +0200

    Added noindexing. To prevent website from showing on google search

--- SyntechShop/EditItems.php
+++ SyntechShop/EditItems.php
@@ -103,6 +103,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="robots" content="noindex">
 	<title>Edit Item</title>
 	<link rel="shortcut icon" href="images/favicon.ico">
 	<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
